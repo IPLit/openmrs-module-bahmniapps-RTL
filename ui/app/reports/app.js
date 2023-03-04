7d69a911074993ebdc6addef8710235a205b34ae
syadav123 <103132639+syadav123@users.noreply.github.com>
2023-03-04 19:21:21 +0530

    date show in field

--- ui/app/reports/app.js
+++ ui/app/reports/app.js
@@ -63,7 +63,7 @@ angular
 
             $bahmniTranslateProvider.init({app: getAppName(), shouldMerge: true});
         }]).run(['$rootScope', '$templateCache', '$window', function ($rootScope, $templateCache, $window) {
-            // moment.locale($window.localStorage["NG_TRANSLATE_LANG_KEY"] || "en");
+            moment.locale($window.localStorage["NG_TRANSLATE_LANG_KEY"] || "en");
             $rootScope.languageUser = $window.localStorage["NG_TRANSLATE_LANG_KEY"] || "en";
             $rootScope.userLanguageDirRtl = false;
             if ($window.localStorage["NG_TRANSLATE_LANG_KEY"] === 'ar') {
