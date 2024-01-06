74593a991b2bb8e7d0a52682d7bbde188e17c215
PranaviMohite11 <109725176+PranaviMohite11@users.noreply.github.com>
2024-01-06 11:16:31 +0530

    UI change on bedmanagement

--- ui/app/bedmanagement/controllers/bedManagementController.js
+++ ui/app/bedmanagement/controllers/bedManagementController.js
@@ -194,6 +194,62 @@ angular.module('bahmni.ipd')
                     className: "ngdialog-theme-default ng-dialog-adt-popUp"
                 });
             };
+            // ward controller code
+            var getSelectedRoom = function (roomName) {
+                var admissionRoom = _.filter($scope.ward.rooms, function (room) {
+                    return room.name === roomName;
+                });
+                $scope.room = admissionRoom[0];
+                $scope.activeRoom = $scope.room.name;
+                $scope.roomSelected = true;
+            };
+            $scope.$on("event:deselectWards", function (event, ward) {
+                $scope.activeRoom = null;
+            });
 
+            var updateSelectedBedInfo = function (roomName) {
+                $rootScope.selectedBedInfo.roomName = roomName;
+                $rootScope.selectedBedInfo.bed = undefined;
+            };
+
+            $scope.onSelectRoom = function (roomName) {
+                if (roomName != undefined) {
+                    updateSelectedBedInfo(roomName);
+                    getSelectedRoom(roomName);
+                    $scope.$emit("event:roomSelected", roomName);
+                    $scope.$broadcast("event:changeBedList", roomName);
+                    $scope.activeRoom = roomName;
+                    goToBedManagement();
+                    if (window.scrollY > 0) {
+                        window.scrollTo(0, 0);
+                    }
+                }
+            };
+
+            var expandAdmissionMasterForRoom = function (roomName) {
+                if (roomName == null) {
+                    updateSelectedBedInfo(roomName);
+                    getSelectedRoom(roomName);
+                }
+            };
+
+            $scope.$on("event:departmentChanged", function (event) {
+                $scope.roomSelected = false;
+            });
+
+            var goToBedManagement = function () {
+                if ($state.current.name === "bedManagement.bed") {
+                    var options = {};
+                    options['context'] = {
+                        department: {
+                            uuid: $scope.ward.uuid,
+                            name: $scope.ward.name
+                        },
+                        roomName: $rootScope.selectedBedInfo.roomName
+                    };
+                    options['dashboardCachebuster'] = Math.random();
+                    $state.go("bedManagement", options);
+                }
+            };
             init();
         }]);
